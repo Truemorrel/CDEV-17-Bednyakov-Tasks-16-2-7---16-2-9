529e6923ae93f1ad098b411b39f71b0564b5c515
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-22 04:24:52 +0300

    master: add task 16.2.7

---
+++ Task 16 2 7.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_16_2_7.Tests
+{
+    public class Class1
+    {
+        private readonly Calculator _calculator = new();
+
+        [Fact]
+        public void SubstructionMustReturnCorrectValue()
+        {
+            Assert.Equal(4, _calculator.Subtraction(6, 2));
+        }
+
+        [Fact]
+        public void FailingSubstructionTest()
+        {
+            Assert.Equal(4, _calculator.Subtraction(6, 2));
+        }
+    }
+}
---
+++ Task 16 2 7/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Task_16_2_7;
+public class Class1
+{
+
+}
